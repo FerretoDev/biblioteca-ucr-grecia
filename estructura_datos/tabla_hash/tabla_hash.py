790695998b91c953104e31a1e385ee756165819f
Marcos Ferreto <marcosferretoestrada@gmail.com>
2026-06-05 16:29:01 -0600

    bug: resolve problem with git

--- estructura_datos/tabla_hash/tabla_hash.py
+++ estructura_datos/tabla_hash/tabla_hash.py
@@ -1,29 +1,26 @@
-from .lista import Lista
-
+from typing import List, Optional
+from estructura_datos.tabla_hash.lista import Lista
 class TablaHash:
-    def __init__(self, tam=97):
-        # Guarda el tamano y crea listas vacias
-        self.tam = tam
-        self.tabla = [None] * tam
-        for i in range(tam):
-            self.tabla[i] = Lista()
+    def __init__(self, tamanio: int) -> None:
+        self.tabla_hash: List[Optional[Lista]] = [None] * tamanio
+        for i in range(tamanio):
+            self.tabla_hash[i] = Lista()
 
-    def calculo_hash(self, carnet):
-        # Usa el numero de carnet (4 digitos) para definir la posicion
-        return int(carnet) % self.tam
+    def calculo_hash(self, clave: int , tam) -> int:
+        suma = 0
+        for char in clave:
+            suma += ord(char)
+        return suma % tam
     
-    def insertar(self, estudiante):
-        # Metemos el estudiante en la lista de acuerdo a su hash
-        index = self.calculo_hash(estudiante.carnet)
-        self.tabla[index].insertar(estudiante)
+    def agregar(self, nombre, tam):
+        index = self.calculo_hash(nombre, tam)
+        self.tabla_hash[index].insertar(nombre)
 
-    def buscar_por_carnet(self, carnet):
-        # Vamos directo al indice y buscamos el carnet
-        index = self.calculo_hash(carnet)
-        return self.tabla[index].buscar_por_carnet(carnet)
-
+    def buscar(self, clave, tam):
+        index = self.calculo_hash(clave, tam)
+        return self.tabla_hash[index].buscar(clave)
     def buscar_por_nombre(self, nombre):
-        # Toca buscar en todas las listas de la tabla
+        # busca en todas las listas de la tabla
         for lista in self.tabla:
             encontrado = lista.buscar_por_nombre(nombre)
             if encontrado is not None:
@@ -40,7 +37,7 @@ class TablaHash:
         return resultado_nombres
 
     def eliminar_estudiante(self, carnet):
-        # Lo eliminamos de la lista correspondiente por carnet
+        # Lo elimina de la lista correspondiente por carnet
         index = self.calculo_hash(carnet)
         return self.tabla[index].eliminar_por_carnet(carnet)
 
